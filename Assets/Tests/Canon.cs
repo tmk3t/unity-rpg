6e46a644502864ab255ac2c70a6ff9bad2f9e98b
kikuta28 <knocknote.kikuta@gmail.com>
2022-02-25 00:42:16 +0900

    SlimeRabbit死んでも動く、リファクタ前

---
+++ Assets/Tests/Canon.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+
+    private float rotateCounter = 0f;
+    private float chargeCounter = 0f;
+    private bool isCharging = false;
+
+    public GameObject target = null;
+
+    public GameObject bulletPrefab = null;
+    public float shootPower = 300f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotateCounter >= 5f)
+        {
+            transform.LookAt(target.transform);
+            isCharging = true;
+            rotateCounter = 0;
+        }
+
+
+        if (chargeCounter >= 2f)
+        {
+            Shoot();
+            isCharging = false;
+            chargeCounter = 0f;
+        }
+
+
+        rotateCounter += Time.deltaTime;
+        if (isCharging == true)
+        {
+            chargeCounter += Time.deltaTime;
+        }
+    }
+
+    public void Shoot()
+    {
+
+        GameObject bullet = Instantiate(bulletPrefab);
+        bullet.transform.position = transform.position;
+        bullet.transform.rotation = transform.rotation;
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * shootPower);
+
+
+    }
+
+
+}
---
+++ Assets/Tests/SlimeRabbitController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlimeRabbitController : MonoBehaviour
+{
+    Animator animator;
+
+    public float moveSpeed = 0.1f;
+    public float rotationSpeed = 8f;
+    public float hitPoint = 100f;
+
+    private float moveDirection;
+    public Rigidbody rb;
+    public Text hitPointText;
+
+    private Camera playerCamera;
+    public Transform viewPoint;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("HitPoint", hitPoint);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = Camera.main;
+
+
+        //fightLayer = animator.GetLayerIndex("Fight");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerCamera.transform.position = viewPoint.position;
+        playerCamera.transform.rotation = viewPoint.rotation;
+
+        animator.SetBool("Move", false);
+        animator.SetFloat("HitPoint", hitPoint);
+        hitPointText.text = "HP: " + animator.GetFloat("HitPoint");
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetBool("Move", true);
+            transform.position = transform.position + transform.forward * moveSpeed;
+            //transform.position = transform.forward * moveSpeed;
+            // animator.SetFloat("Speed", 0.2f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.SetBool("Move", true);
+            transform.position = transform.position + transform.forward * moveSpeed * -1;
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetBool("Move", true);
+            //transform.Rotate(0f, 10f, 0f);
+            moveDirection = 2f;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetBool("Move", true);
+            //transform.Rotate(0f, -10f, 0f);
+            moveDirection = -2f;
+
+        }
+        else
+        {
+            moveDirection = 0f;
+        }
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + moveDirection, transform.rotation.eulerAngles.z);
+
+        if (animator.GetFloat("HitPoint") <= 0f)
+        {
+            animator.SetTrigger("Death");
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit! 1");
+        if (collision.gameObject.tag == "DamageObject")
+        {
+            Debug.Log("Damage!");
+            //rb.constraints = RigidbodyConstraints.FreezePositionY;
+            //rb.constraints = RigidbodyConstraints.FreezeRotation;
+            //rb.constraints = RigidbodyConstraints.FreezeRotation;
+            //rb.constraints = RigidbodyConstraints.FreezeRotation;
+            animator.SetBool("Damage", true);
+            hitPoint -= 50f;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Hit! 3");
+        if (collision.gameObject.tag == "DamageObject")
+        {
+            Destroy(collision.gameObject);
+        }
+
+
+    }
+
+
+}
