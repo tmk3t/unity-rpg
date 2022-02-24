ae19c91e049ecc0afbe0d5b7de8d7b039d47d5c3
kikuta28 <knocknote.kikuta@gmail.com>
2022-02-25 00:52:51 +0900

    死んだら止まる

--- Assets/Tests/SlimeRabbitController.cs
+++ Assets/Tests/SlimeRabbitController.cs
@@ -7,69 +7,84 @@ public class SlimeRabbitController : MonoBehaviour
 {
     Animator animator;
 
+    public Rigidbody rb;
+    public Text hitPointText;
+    public Transform viewPoint;
+
     public float moveSpeed = 0.1f;
     public float rotationSpeed = 8f;
     public float hitPoint = 100f;
 
-    private float moveDirection;
-    public Rigidbody rb;
-    public Text hitPointText;
+    private float moveDirection = 0f;
+
+    private bool isDead = false;
+
+
 
     private Camera playerCamera;
-    public Transform viewPoint;
 
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-        animator.SetFloat("HitPoint", hitPoint);
-    }
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("HitPoint", hitPoint);
         playerCamera = Camera.main;
-
-
-        //fightLayer = animator.GetLayerIndex("Fight");
-
-
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        playerCamera.transform.position = viewPoint.position;
-        playerCamera.transform.rotation = viewPoint.rotation;
+        CameraUpdate();
 
         animator.SetBool("Move", false);
         animator.SetFloat("HitPoint", hitPoint);
         hitPointText.text = "HP: " + animator.GetFloat("HitPoint");
 
+        if (isDead != true)
+        {
+            Move();
+        }
+
+
+        if (animator.GetFloat("HitPoint") <= 0f)
+        {
+            animator.SetTrigger("Death");
+            isDead = true;
+        }
+
+    }
+
+    void CameraUpdate()
+    {
+        playerCamera.transform.position = viewPoint.position;
+        playerCamera.transform.rotation = viewPoint.rotation;
+    }
+
+
+    void Move()
+    {
         if (Input.GetKey(KeyCode.UpArrow))
         {
             animator.SetBool("Move", true);
             transform.position = transform.position + transform.forward * moveSpeed;
-            //transform.position = transform.forward * moveSpeed;
-            // animator.SetFloat("Speed", 0.2f);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             animator.SetBool("Move", true);
             transform.position = transform.position + transform.forward * moveSpeed * -1;
-
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             animator.SetBool("Move", true);
-            //transform.Rotate(0f, 10f, 0f);
             moveDirection = 2f;
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             animator.SetBool("Move", true);
-            //transform.Rotate(0f, -10f, 0f);
             moveDirection = -2f;
-
         }
         else
         {
@@ -77,36 +92,15 @@ public class SlimeRabbitController : MonoBehaviour
         }
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + moveDirection, transform.rotation.eulerAngles.z);
 
-        if (animator.GetFloat("HitPoint") <= 0f)
-        {
-            animator.SetTrigger("Death");
-        }
-
     }
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Hit! 1");
         if (collision.gameObject.tag == "DamageObject")
         {
-            Debug.Log("Damage!");
-            //rb.constraints = RigidbodyConstraints.FreezePositionY;
-            //rb.constraints = RigidbodyConstraints.FreezeRotation;
-            //rb.constraints = RigidbodyConstraints.FreezeRotation;
-            //rb.constraints = RigidbodyConstraints.FreezeRotation;
             animator.SetBool("Damage", true);
             hitPoint -= 50f;
-        }
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        Debug.Log("Hit! 3");
-        if (collision.gameObject.tag == "DamageObject")
-        {
             Destroy(collision.gameObject);
         }
-
-
     }
-
 
 }
